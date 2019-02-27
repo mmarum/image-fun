3e04d1cdf512b2c245a549f5d2ba6435fc35ba1c
Matt Marum <mmarum1@advancelocal.net>
2019-02-27 15:50:34 -0500

    sideways

--- convert.py
+++ convert.py
@@ -1,29 +1,40 @@
 #!/usr/bin/env python
 
+#import os
+#import json
+
 from PIL import Image
 
-im = Image.open("selfie-200.png")   
+im = Image.open("selfie-100.png")   
 
-out = Image.new('I', im.size, 0xffffff)
+#out = Image.new('I', im.size, 0xffffff)
+out = Image.new('P', im.size, 0xffffff)
 
 width, height = im.size
 
 i=1
-
+pixel_colors = []
 for x in range(width):
-
     for y in range(height):
+        r,g,b,a = im.getpixel((x,y))
 
-        #r,g,b = im.getpixel((x,y))
+        pixel_color_tuple = (r, g, b)
+
+        p = im.getpixel((x,y))
+
+        print(x)
+        print(y)
         print(i)
-        print(im.getpixel((x,y)))
+        print(p)
 
-        #if b < g and b < r or r==g==b:
+        out.putpixel((x,y), g)
+
+        pixel_colors.append(str(pixel_color_tuple))
 
         i += 1
 
-        out.putpixel((x,y), 0)
+out.save('bar.png')
 
-#            #out.putpixel((x,y), 0)
-
-#out.save('bar.png')
+f = open('bar.json', "w")
+f.write('bar_pixels='+str(pixel_colors))
+f.close()
